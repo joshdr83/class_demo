dd04520e8a1b961ff1cfd22a35ba965488c61037
Joshua Rhodes <joshdr83@gmail.com>
2024-10-27 14:33:44 -0500

    first commit

---
+++ PV_watts_OG.py
@@ -0,0 +1,20 @@
+import sys, json, requests
+from urllib.request import urlopen
+
+# input parameters
+lat = sys.argv[1]
+long = sys.argv[2]
+size = sys.argv[3]
+
+# url generator
+nrel_api_key = 'kVTh5O88nbYohgldbB92JC17TL4UzxwauQo1x2Ha'
+
+url = 'https://developer.nrel.gov/api/pvwatts/v6.json?api_key=%s&lat=%s&lon=%s&system_capacity=%s&azimuth=180&tilt=40&array_type=1&module_type=1&losses=10' % (nrel_api_key, lat, long, size)
+
+response = urlopen(url)
+pv_out = json.loads(response.read())
+
+location = pv_out['station_info']['state']
+gen = round(pv_out['outputs']['ac_annual'])
+
+print('A %s kW solar PV array in %s should output about %s kWh per year!' % (size, location, gen))
